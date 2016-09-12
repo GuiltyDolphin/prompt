2979b5a62dfa957b5966a53ce96f82940f3870f5
Ben Moon <guiltydolphin@gmail.com>
2016-09-12 13:31:38 +0100

    Add 'prompt-completing-read-variable

--- prompt.el
+++ prompt.el
@@ -35,6 +35,11 @@
   :group 'minibuffer
   :prefix 'prompt-)
 
+(defcustom prompt-default-prompt-suffixes '(":" "?")
+  "Default accepted prompt suffixes."
+  :group 'prompt
+  :type '(repeat string))
+
 (defun prompt--split (prompt suffixes)
   "Split PROMPT into its main body and suffix in the form (BODY SUFFIX).
 SUFFIXES should be a list of possible suffixes."
@@ -50,6 +55,29 @@ SUFFIXES should be a list of possible suffixes."
 DEFAULT should be the default value to indicate in the prompt, or NIL for no default value."
   (-let (((pbody psuffix) (prompt--split prompt suffixes)))
     (format "%s%s%s " pbody (if default (format " (default %s)" default) "") psuffix)))
+
+(defun prompt--make-interactive-prompt (prompt default)
+  "Normalize PROMPT using `prompt-default-prompt-suffixes' as the suffixes.
+DEFAULT should be the default value to indicate in the prompt, or NIL for no default value."
+  (prompt--make-prompt prompt prompt-default-prompt-suffixes default))
+
+(defun prompt-completing-read-variable (prompt &optional predicate def)
+  "Read the name of a variable in the minibuffer, with completion.
+See `completing-read' for the meaning of PROMPT, PREDICATE, and DEF."
+  (let ((v (or def (variable-at-point)))
+        (enable-recursive-minibuffers t)
+        (check
+         (lambda (it)
+           (and (symbolp it)
+                (boundp it)
+                (if predicate (funcall predicate (symbol-value it)) t))))
+        vars val)
+    (mapatoms (lambda (atom) (when (funcall check atom) (push atom vars))))
+    (setq val (completing-read
+               (prompt--make-interactive-prompt prompt (when (funcall check v) v))
+               vars check t nil nil
+               (if (symbolp v) (symbol-name v))))
+    (list (if (equal val "") v (intern val)))))
 
 (provide 'prompt)
 ;;; prompt.el ends here
